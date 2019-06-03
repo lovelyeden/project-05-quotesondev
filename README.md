4f347f108e98e74325b53e64af93378ec4017fa5
lovely <ldde07@gmail.com>
2019-06-03 06:55:15 -0700

    Remove unnecessary comments

--- README.md
+++ README.md
@@ -1,12 +1,19 @@
 # Quotes on Dev Starter
 
-A WordPress starter theme for the Quotes on Dev project, forked from Underscores.
+A WordPress starter theme for the Quotes on Dev project.
+It is a combination of JavaScript and WordPress skills together to build out the functionality for a website called Quotes on Dev using the WordPress REST API.
 
-## Installation
+## Technologies 
+wordpress - website content management system
+php -is a server side scripting language used to develop static websites or dynamic websites or web applications
+npm - to generate a package.json file
+gulp - tools for automating tasks
+jquery - a javascript library
+ajax - a way to exchange data between the browser and the server without reloading the page
+json -a way to encode both simple and more complex data structures
+api - user interfaces allow people to work with programs
+sass - preprocessor scripting language that is interpreted or compiled into Cascading Style Sheets (CSS)
 
-### 1. Download me (don't clone me!)
-
-Then add me to your `wp-content/themes` directory.
 
 ### 2. Rename the `quotesondev-starter-master` directory
 
--- single.php
+++ single.php
@@ -7,32 +7,28 @@
 
 get_header(); ?>
 
-	<div id="primary" class="content-area">
-		<main id="main" class="site-main" role="main">
-
+<div id="primary" class="content-area">
+	<main id="main" class="site-main" role="main">
 		<?php while ( have_posts() ) : the_post(); ?>
-
 			<?php get_template_part( 'template-parts/content', 'single' ); ?>
+		<?php endwhile;?>
+	</main>
+</div>
 
-		<?php endwhile; // End of the loop. ?>
-
-		</main><!-- #main -->
-	</div><!-- #primary -->
-
-	<div class="entry-meta">
-		<?php the_title('<h2 class="entry-title">&mdash;', '<h2>'); ?>
-		<?php if( $source && $source_url ): ?>
-			<span class="source">,
-				<a href="<?php echo $source_url; ?>">
-				<?php echo $source; ?>
-				</a>
-			</span>
+<div class="entry-meta">
+	<?php the_title('<h2 class="entry-title">&mdash;', '<h2>'); ?>
+	<?php if( $source && $source_url ): ?>
+		<span class="source">,
+			<a href="<?php echo $source_url; ?>">
+			<?php echo $source; ?>
+			</a>
+		</span>
 		
 		<?php elseif ( $source ): ?>
 			<span class="source"><?php echo $source; ?></span>
 		<?php else: ?>
 			<span class="source"></span>
 		<?php endif; ?>
-	</div> 
+</div> 
 <button type="button" id="new-quote-button">Show Me Another!</button>
 <?php get_footer(); ?>
